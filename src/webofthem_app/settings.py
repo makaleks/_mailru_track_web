b0dc709054ca781b395c3f8a7de1b2e8bb0f49dc
makaleks <makaleks@live.ru>
2016-09-29 17:10:26 +0300

    Apps installed.

--- src/webofthem_app/settings.py
+++ src/webofthem_app/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webalbum',
+    'webchat',
+    'webcomment',
+    'weblike',
+    'webmessage',
+    'webphoto',
+    'webpost',
+    'webuser',
 ]
 
 MIDDLEWARE = [
