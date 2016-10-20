c140049931236f115ae47c91b8ab435c41414afb
makaleks <makaleks@live.ru>
2016-10-20 20:40:05 +0300

    Source (example) code from webpost removed.

--- src/webpost/views.py
+++ src/webpost/views.py
@@ -11,14 +11,3 @@ from django.contrib.auth import get_user_model
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly)
-
-    def get_queryset(self):
-        qs = super(PostViewSet, self).get_queryset()
-        if self.request.query_params.get('username'):
-            qs = qs.filter(author__username=self.request.query_params.get('username'))
-        return qs
-
-    def perform_create(self, serializer):
-        auth_type = ContentType.objects.get_for_model(get_user_model())
-        serializer.save(author_id=self.request.user, author_type=auth_type)
