c479fc91b970c9b8da8a50511b1fa448bdc695cd
makaleks <makaleks@live.ru>
2017-02-09 11:41:48 +0300

    Appeal is adopted to different screens!

--- collected_static/base.js
+++ collected_static/base.js
@@ -1,53 +1,34 @@
-
-function set_menu_large() {
-    $(document).foundation();
-    var elem = new Foundation.Sticky($('#id-menu'), {
-        //dataAnchor: content_id
-        marginTop: 0
-    });
-    //$('.sticky').foundation('_calc', true);
-    //$('.sticky').foundation('_setSticky');
-
-    //$('.sticky').foundation('_events', true);
-    //Foundation.reInit('sticky');
-    //Foundation.reInit('sticky');
-    //$(document).foundation();
-    //$(document).foundation('reflow');
+function set_menu_small() {
+    // Move menu
+    $('#id-menu-container').appendTo('#id-off-canvas');
+    // Remove margin
+    $('#id-menu-container').removeClass('medium-2 column');
+    // In other case content width is 10/12, floating right
+    $('#id-content').removeClass('medium-10 column'); 
 }
 
-function set_menu_canvas() {
-    $('.sticky').foundation('_removeSticky');
-
-    var elem = new Foundation.OffCanvas($('#id-offcanvas'), {
-        //dataAnchor: content_id
-        marginTop: 0
-    });
-    //$('.sticky').foundation('_calc', true);
-    //$('.sticky').foundation('_setSticky');
-    
-    //$('.sticky').foundation('_events', true);
-    //Foundation.reInit('sticky');
-    //Foundation.reInit('sticky');
-    //$(document).foundation();
-    //$(document).foundation('reflow');
+function set_menu_large() {
+    $('#id-menu-container').insertBefore('#id-content');
+    $('#id-menu-container').addClass('medium-2 column');
+    $('#id-content').addClass('medium-10 column');
+    $('#id-off-canvas').foundation('close');
 }
 
 function size_breakpoint_callback_menu(event, newSize, oldSize) {
-    console.log("MYEVENT", event, newSize, oldSize);
+    // console.log("MYEVENT", event, newSize, oldSize);
     if(newSize != "small") {
         set_menu_large();
     }
     else {
-        //set_menu_canvas();
+        set_menu_small();
     }
 }
 
-$(function() {
+$(function() { // Called after all DOM is loaded to jQuery
     $(document).foundation();
-
     size_breakpoint_callback_menu(
-            null, Foundation.MediaQuery.current, null);
-    //set_menu_large();
+          null, Foundation.MediaQuery.current, null);
 })
 
+// Set callback on screen size change
 $(window).on('changed.zf.mediaquery', size_breakpoint_callback_menu);
